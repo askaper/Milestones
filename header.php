22048e9936152cacfa99598721666c5906e86338
Andrew Skaper <andrew@skaper.info>
2015-09-19 22:22:27 -0700

    Initial Milestone 1 push

---
+++ header.php
@@ -0,0 +1,11 @@
+<header><h2>Business Site</h2>
+	<nav>
+		<a href="/index.php">Home |</a>
+		<a href="/contact.php">Contact |</a>
+		<a href="/product.php">Browse Products</a>
+	</nav>
+</header>
+
+
+
+	
---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<link rel="stylesheet" href="css/style.css">
+	<meta charset="UTF-8">
+	<title>Business Site</title>
+</head>
+<body>
+	<?php include 'header.php';?>
+</body>
+</html>
