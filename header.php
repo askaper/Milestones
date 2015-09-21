db1ec3e5ed1e06bbdc6e778058c87e75aa1bd0d2
Andrew Skaper <andrew@skaper.info>
2015-09-21 16:38:31 -0700

    aasdf

--- header.php
+++ header.php
@@ -1,14 +1,14 @@
 <header>
 	
-	<a href="/index.php"><p class="logo">Business Site</p></a>
+	<a href="index.php"><p class="logo">Business Site</p></a>
 	
 	<nav>
 
-		<a href="/index.php">Home</a>
+		<a href="index.php">Home</a>
 		<p>|</p>
-		<a href="/contact.php">Contact</a>
+		<a href="contact.php">Contact</a>
 		<p>|</p>
-		<a href="/browse.php">Browse Products</a>
+		<a href="browse.php">Browse Products</a>
 	
 	</nav>
 </header>
